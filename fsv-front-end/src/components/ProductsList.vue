46e2fb29aa4d384686ba3c27542240fe4b765e89
CJ Vaughn <cj.vaughn052284@gmail.com>
2021-10-29 14:26:40 -0700

    Made Cart Page more modular

---
+++ fsv-front-end/src/components/ProductsList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ProductsListItem v-for="item in productList" :key="item.id" :item="item" />
+  </div>
+</template>
+
+<script>
+import ProductsListItem from "./ProductsListItem.vue";
+
+export default {
+  name: "ProductsList",
+  components: {
+    ProductsListItem,
+  },
+  props: ["productList"],
+};
+</script>
+
+<style scoped>
+</style>
--- fsv-front-end/src/views/CartPage.vue
+++ fsv-front-end/src/views/CartPage.vue
@@ -1,14 +1,7 @@
 <template>
   <div id="page-wrap">
     <h1>Shopping Cart</h1>
-    <div v-for="item in cartItems" :key="item.id" class="product-container">
-      <img class="product-image" :src="item.imageUrl" />
-      <div class="details-wrap">
-        <h3>{{ item.name }}</h3>
-        <p>${{ item.price }}</p>
-      </div>
-      <button class="remove-button">Remove From Cart</button>
-    </div>
+    <ProductsList :productList="cartItems" />
     <h3 id="total-price">Total: ${{ totalPrice }}</h3>
     <button id="checkout-button">Proceed to Checkout</button>
   </div>
@@ -16,9 +9,13 @@
 
 <script>
 import { cartItems } from "../fake-data";
+import ProductsList from "../components/ProductsList.vue";
 
 export default {
   name: "CartPage",
+  components: {
+    ProductsList,
+  },
   data() {
     return {
       cartItems,
@@ -47,29 +44,5 @@ h1 {
 
 #checkout-button {
   width: 100%;
-}
-
-.product-container {
-  align-content: "center";
-  border-bottom: 1px solid #ddd;
-  display: flex;
-  padding: 16px;
-  width: 100%;
-}
-
-.product-image {
-  flex: 1;
-  height: 100px;
-  max-width: 100px;
-}
-
-.details-wrap {
-  padding: 0 16px;
-  flex: 3;
-}
-
-.remove-button {
-  flex: 1;
-  margin: auto;
 }
 </style>
